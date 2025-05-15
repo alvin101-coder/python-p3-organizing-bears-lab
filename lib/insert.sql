54b6540d5240c1aaa9245d1b78ae1d3b7df7bed7
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-05-15 18:32:35 +0300

    Implemented SQL queries for selecting bears

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'aggressive', 1),
+  ('Rowdy', 3, 'M', 'black', 'friendly', 1),
+  ('Tabitha', 2, 'F', 'golden', 'calm', 1),
+  ('Sergeant Brown', 7, 'M', 'brown', 'aggressive', 1),
+  ('Melissa', 4, 'F', 'brown', 'curious', 1),
+  ('Grinch', 6, 'M', 'green', 'mischievous', 1),
+  ('Wendy', 1, 'F', 'white', 'playful', 1),
+  (NULL, 3, 'M', 'gray', 'neutral', 1);  -- unnamed bear
